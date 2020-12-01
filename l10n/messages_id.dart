d3907f01ba0d24e0d52a381fbdbf689b06861208
AthenaPoison <69492597+AthenaPoison@users.noreply.github.com>
2020-12-01 12:47:14 +0800

    Add files via upload

---
+++ l10n/messages_id.dart
@@ -0,0 +1,31 @@
+// DO NOT EDIT. This is code generated via package:intl/generate_localized.dart
+// This is a library that provides messages for a id locale. All the
+// messages from the main program should be duplicated here with the same
+// function name.
+
+// Ignore issues from commonly used lints in this file.
+// ignore_for_file:unnecessary_brace_in_string_interps, unnecessary_new
+// ignore_for_file:prefer_single_quotes,comment_references, directives_ordering
+// ignore_for_file:annotate_overrides,prefer_generic_function_type_aliases
+// ignore_for_file:unused_import, file_names
+
+import 'package:intl/intl.dart';
+import 'package:intl/message_lookup_by_library.dart';
+
+final messages = new MessageLookup();
+
+typedef String MessageIfAbsent(String messageStr, List<dynamic> args);
+
+class MessageLookup extends MessageLookupByLibrary {
+  String get localeName => 'id';
+
+  final messages = _notInlinedMessages(_notInlinedMessages);
+  static _notInlinedMessages(_) => <String, Function> {
+    "langEN" : MessageLookupByLibrary.simpleMessage("Bahasa Inggris"),
+    "langFP" : MessageLookupByLibrary.simpleMessage("Bahasa Tagalog"),
+    "langID" : MessageLookupByLibrary.simpleMessage("Bahasa Indonesia"),
+    "langMS" : MessageLookupByLibrary.simpleMessage("Bahasa Melayu"),
+    "langSetting" : MessageLookupByLibrary.simpleMessage("Mengubah Bahasa"),
+    "langZH" : MessageLookupByLibrary.simpleMessage("Bahasa Cina")
+  };
+}
